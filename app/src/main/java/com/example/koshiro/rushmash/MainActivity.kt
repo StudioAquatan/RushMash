0ab8d93fee6dc9ab8020758f8ef4a4d1c0750e91
lanqiu <ksumebosi@gmail.com>
2018-06-16 17:21:38 +0900

    MainActivityのツールバー追加
    ツールバーにアイコンをつけてクリックすると簡単な表示をする

--- app/src/main/java/com/example/koshiro/rushmash/MainActivity.kt
+++ app/src/main/java/com/example/koshiro/rushmash/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.koshiro.rushmash.R
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground)
+
+        toolbar.setNavigationOnClickListener {
+            Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+        }
 
         addListButton.setOnClickListener {
 
