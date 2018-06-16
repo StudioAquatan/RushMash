c7371bf5633d8db95e805b0b54d12cd00e3bc2d2
shanpu <cheztomo513@gmail.com>
2018-06-17 04:26:12 +0900

    アラーム音とその再生を追加

--- app/src/main/java/com/example/koshiro/rushmash/AlarmActivity.kt
+++ app/src/main/java/com/example/koshiro/rushmash/AlarmActivity.kt
@@ -1,25 +1,53 @@
 package com.example.koshiro.rushmash
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_alarm.*
 
 class AlarmActivity : AppCompatActivity() {
+
+    companion object {
+        private var qikokuFlag: Boolean = false
+    }
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
 
-        if(intent.getStringExtra("DEMO_SECOND_TIME") != null) {
+        if (intent.getStringExtra("DEMO_SECOND_TIME") != null) {
             time_textview.text = intent.getStringExtra("DEMO_SECOND_TIME")
         }
 
         stop_alarm_button.setOnClickListener {
+            audioStop()
             val intent = Intent(this, ScheduleActivity::class.java)
             startActivity(intent)
         }
+
+        if (!qikokuFlag) {
+            player = MediaPlayer.create(this, R.raw.alarm)
+        } else {
+            player = MediaPlayer.create(this, R.raw.alarm_qikoku)
+        }
+        player!!.isLooping = true
+        player!!.start()
+        Log.d("music","music start")
+        Log.d("flag","qikokuFlag is $qikokuFlag")
+        qikokuFlag = !qikokuFlag
     }
 
-
+    fun audioStop() {
+        if (player != null) {
+            player.apply {
+                this!!.stop()
+                reset()
+                release()
+            }
+            player = null
+        }
+    }
 }
