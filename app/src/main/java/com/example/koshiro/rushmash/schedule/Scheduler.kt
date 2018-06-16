1ffb3193ea557b615501310f6a4e3ee4695d1fe3
taxio <taxio.2029@gmail.com>
2018-06-16 23:56:23 +0900

    add mock data

--- app/src/main/java/com/example/koshiro/rushmash/schedule/Scheduler.kt
+++ app/src/main/java/com/example/koshiro/rushmash/schedule/Scheduler.kt
@@ -6,7 +6,7 @@ enum class MockPriority {
     High, Mid, Low
 }
 
-data class MockToDo (
+data class MockToDo(
         var id: Int,
         var name: String,
         var duration: Int,
@@ -14,9 +14,48 @@ data class MockToDo (
         var category: Int
 )
 
+var mockData: MutableList<MockToDo> = mutableListOf(
+        MockToDo(
+                id = 1,
+                name = "シャワー",
+                duration = 10,
+                mockPriority = MockPriority.High,
+                category = 0),
+        MockToDo(
+                id = 2,
+                name = "歯磨き",
+                duration = 3,
+                mockPriority = MockPriority.Low,
+                category = 0),
+        MockToDo(
+                id = 3,
+                name = "髪セット",
+                duration = 5,
+                mockPriority = MockPriority.Low,
+                category = 0),
+        MockToDo(
+                id = 4,
+                name = "着替え",
+                duration = 3,
+                mockPriority = MockPriority.High,
+                category = 0),
+        MockToDo(
+                id = 5,
+                name = "トイレ",
+                duration = 10,
+                mockPriority = MockPriority.Low,
+                category = 0),
+        MockToDo(
+                id = 6,
+                name = "荷物用意",
+                duration = 5,
+                mockPriority = MockPriority.High,
+                category = 0)
+)
+
 class Scheduler {
 
-    fun optimizeSchedule(remineTime: Int, schedules: MutableList<MockToDo>): MutableList<MockToDo>{
+    fun optimizeSchedule(remineTime: Int, schedules: MutableList<MockToDo>): MutableList<MockToDo> {
         /*
             削除優先
          */
@@ -34,26 +73,26 @@ class Scheduler {
 
         // PriorityがLowの中から削除
         var lows = schedules.filter { it.mockPriority == MockPriority.Low }
-        var lowsSorted = lows.sortedBy { abs(forDeleteTime-it.duration) }
+        var lowsSorted = lows.sortedBy { abs(forDeleteTime - it.duration) }
         print("low sorted : ")
         println(lowsSorted)
-        for (l in lowsSorted){
+        for (l in lowsSorted) {
             schedules.dropWhile { it.id == l.id }
             forDeleteTime -= l.duration
-            if (forDeleteTime <= 0){
+            if (forDeleteTime <= 0) {
                 return schedules
             }
         }
 
         // PriorityがMidの中から削除
         var mids = schedules.filter { it.mockPriority == MockPriority.Mid }
-        var midsSorted = mids.sortedBy { abs(forDeleteTime-it.duration) }
+        var midsSorted = mids.sortedBy { abs(forDeleteTime - it.duration) }
         print("mid sorted : ")
         println(midsSorted)
-        for (m in midsSorted){
+        for (m in midsSorted) {
             schedules.dropWhile { it.id == m.id }
             forDeleteTime -= m.duration
-            if (forDeleteTime <= 0){
+            if (forDeleteTime <= 0) {
                 return schedules
             }
         }
@@ -62,23 +101,23 @@ class Scheduler {
         return mutableListOf()
     }
 
-    fun attachCategory(schedules: MutableList<MockToDo>): MutableList<MockToDo>{
+    fun attachCategory(schedules: MutableList<MockToDo>): MutableList<MockToDo> {
         var durationSum: Int = 0
         schedules.map { durationSum += it.duration }
         var categoryTime: Int = durationSum / 3
 
         var tmp: Int = 0
         var cat = 0
-        for (sch in schedules){
+        for (sch in schedules) {
             tmp += sch.duration
-            if (tmp >= categoryTime){
-                var diff1 = categoryTime - (tmp-sch.duration)
+            if (tmp >= categoryTime) {
+                var diff1 = categoryTime - (tmp - sch.duration)
                 var diff2 = tmp - categoryTime
-                if (diff1 <= diff2){
+                if (diff1 <= diff2) {
                     cat++
                     sch.category = cat
                     tmp = sch.duration
-                }else{
+                } else {
                     sch.category = cat
                     cat++
                     tmp = 0
