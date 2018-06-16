560478606b1b70ca64a1f1c0a90c7a514af74c3c
taxio <taxio.2029@gmail.com>
2018-06-17 01:01:12 +0900

    bug fix
    最適化計算のバグを修正

--- app/src/main/java/com/example/koshiro/rushmash/schedule/Scheduler.kt
+++ app/src/main/java/com/example/koshiro/rushmash/schedule/Scheduler.kt
@@ -14,7 +14,7 @@ data class MockToDo(
         var category: Int
 )
 
-var mockData: MutableList<MockToDo> = mutableListOf(
+var mockData: List<MockToDo> = listOf(
         MockToDo(
                 id = 1,
                 name = "朝食",
@@ -25,7 +25,7 @@ var mockData: MutableList<MockToDo> = mutableListOf(
                 id = 2,
                 name = "シャワー",
                 duration = 20,
-                priority = 1,
+                priority = 2,
                 category = 0),
         MockToDo(
                 id = 3,
@@ -61,45 +61,37 @@ var mockData: MutableList<MockToDo> = mutableListOf(
 
 class Scheduler {
 
-    fun optimizeSchedule(remineTime: Int, schedules: MutableList<MockToDo>): MutableList<MockToDo> {
-        /*
-            削除優先
-         */
+    fun optimizeSchedule(remineTime: Int, schedules: List<MockToDo>): List<MockToDo> {
         var durationSum: Int = 0
         schedules.map { durationSum += it.duration }
         println(durationSum)
         if (remineTime >= durationSum) {
-            println("通常の予定通りです")
             return schedules
         }
 
-        println("残り時間が足りないので予定を削ります")
         // 削除する時間を計算
         var forDeleteTime = durationSum - remineTime
+        var optimized: List<MockToDo> = schedules
 
         // PriorityがLowの中から削除
         var lows = schedules.filter { it.priority == 2 }
         var lowsSorted = lows.sortedBy { abs(forDeleteTime - it.duration) }
-        print("low sorted : ")
-        println(lowsSorted)
         for (l in lowsSorted) {
-            schedules.dropWhile { it.id == l.id }
+            optimized = optimized.filter { it.id != l.id }
             forDeleteTime -= l.duration
             if (forDeleteTime <= 0) {
-                return schedules
+                return optimized
             }
         }
 
         // PriorityがMidの中から削除
         var mids = schedules.filter { it.priority == 1 }
         var midsSorted = mids.sortedBy { abs(forDeleteTime - it.duration) }
-        print("mid sorted : ")
-        println(midsSorted)
         for (m in midsSorted) {
-            schedules.dropWhile { it.id == m.id }
+            optimized = optimized.filter { it.id != m.id }
             forDeleteTime -= m.duration
             if (forDeleteTime <= 0) {
-                return schedules
+                return optimized
             }
         }
 
@@ -107,7 +99,7 @@ class Scheduler {
         return mutableListOf()
     }
 
-    fun attachCategory(schedules: MutableList<MockToDo>): MutableList<MockToDo> {
+    fun attachCategory(schedules: List<MockToDo>): List<MockToDo> {
         var durationSum: Int = 0
         schedules.map { durationSum += it.duration }
         var categoryTime: Int = durationSum / 3
@@ -119,14 +111,18 @@ class Scheduler {
             if (tmp >= categoryTime) {
                 var diff1 = categoryTime - (tmp - sch.duration)
                 var diff2 = tmp - categoryTime
-                if (diff1 <= diff2) {
-                    cat++
+                if (cat == 2){
                     sch.category = cat
-                    tmp = sch.duration
-                } else {
-                    sch.category = cat
-                    cat++
-                    tmp = 0
+                }else{
+                    if (diff1 <= diff2) {
+                        cat++
+                        sch.category = cat
+                        tmp = sch.duration
+                    } else {
+                        sch.category = cat
+                        cat++
+                        tmp = 0
+                    }
                 }
             }
             sch.category = cat
